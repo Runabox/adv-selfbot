14e29975c322ca56e58d17d7fc106567c0f29977
Runabox <murrellcooper@gmail.com>
2021-09-09 21:22:51 -0400

    ass

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = require('discord.js');
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], partials: ["CHANNEL"] });
 const config = require('./config.json');
 
-const idCache = {};
+const idCache = [];
 
 client.on('ready', () => {
     // Logged into discord gateway
@@ -12,13 +12,14 @@ client.on('ready', () => {
         Send message in disboard channel stating that the bot has started 
         (make sure this channel is private in your server)
     */
-    const channel = client.channels.cache.get(config.disboard.channelID);
+    /*const channel = client.channels.cache.get(config.disboard.channelID);
     channel.send('Advertising bot started');
+    */
 
-    atdm();
+    //atdm();
 });
 
-// Set interval for bump message
+/*// Set interval for bump message
 setInterval(() => {
     // Send bump message
     const channel = client.channels.cache.get(config.disboard.channelID);
@@ -28,7 +29,7 @@ setInterval(() => {
 // Set interval for advertising message
 setInterval(() => {
     atdm();
-}, (1200 * 1000));
+}, (1200 * 1000));*/
 
 client.on('messageCreate', (msg) => {
     // idk
